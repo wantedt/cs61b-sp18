526690917236fedca597329803d8f69287e5dc03
Yu Zhou <yuzhou6066@gmail.com>
2022-04-29 14:31:56 -0400

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,10 +1,14 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ *
+ * @author YOUR NAME HERE
  */
 public class LeapYear {
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    /**
+     * Calls isLeapYear to print correct statement.
+     *
+     * @param year to be analyzed
      */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
@@ -14,7 +18,19 @@ public class LeapYear {
         }
     }
 
-    /** Must be provided an integer as a command line argument ARGS. */
+    /**
+     * class isLeapYear is to test if the year is leapYear
+     *
+     * @param year to be analyzed
+     * @return true or false
+     */
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
+    }
+
+    /**
+     * Must be provided an integer as a command line argument ARGS.
+     */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
